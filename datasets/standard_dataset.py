add average meter and fix onehot label encoder

datasets/standard_dataset.py
@@ -92,10 +92,7 @@ class StandardDataset(Dataset):
         image = self.to_tensor(image)
         mask = self.__get_mask_tensor(mask)
         onehot_mask = self.__get_onehot_mask(mask)
-        return EasyDict(dict(
-            original_image=original_image, original_mask=original_mask,
-            image=image, mask=mask, onehot_mask=onehot_mask
-        ))
+        return EasyDict(dict(image=image, mask=mask, onehot_mask=onehot_mask))
     
     def __get_mask_tensor(self, mask):
         r"""Convert ndarray mask to torch.tensor mask"""
@@ -104,7 +101,7 @@ class StandardDataset(Dataset):
     
     def __get_onehot_mask(self, mask):
         r"""Get onehot mask from integer mask"""
-        onehot_mask = F.one_hot(mask)
+        onehot_mask = F.one_hot(mask, num_classes=22)
         onehot_mask = onehot_mask.permute(2, 0, 1)
         onehot_mask = onehot_mask[:-1]
         return onehot_mask

metrics.py
@@ -9,16 +9,54 @@ def CELoss(y_pred, onehot_masks, eps=1e-9):
 
     Args:
         y_pred (torch.tensor): Model prediction, which is a tensor of shape (n_samples, n_class, h, w).
-        masks (torch.tensor): Groundtruth, which is a tensor of shape (n_samples, n_class, h, w).
+        onehot_masks (torch.tensor): Groundtruth, which is a tensor of shape (n_samples, n_class, h, w).
     Returns
-        torch.tensor: (float) loss value
+        torch.tensor: (float) loss value.
     """
     y_pred = F.softmax(y_pred, dim=1)
     loss = torch.sum(- onehot_masks * torch.log(y_pred + eps), dim=1).mean()
     return loss
 
+def pixel_acc(self, y_pred, onehot_masks):
+    r"""Compute pixel accuracy of segmentation result
+
+    Args:
+        y_pred (torch.tensor): Model prediction, which is a tensor of shape (n_samples, n_class, h, w).
+        onehot_masks (torch.tensor): Groundtruth, which is a tensor of shape (n_samples, n_class, h, w).
+    Returns
+        torch.tensor: (float) pixel accuracy value.
+    """
+    preds = torch.argmax(y_pred, axis=1)
+    label = torch.argmax(onehot_masks, axis=1)
+    acc_sum = torch.sum((preds == label).long())
+    pixel_sum = label.numel()
+    acc = acc_sum.float() / (pixel_sum.float() + 1e-10)
+    return acc
+
+def intersectionAndUnion(y_pred, onehot_masks):
+    r"""Compute intersection and union of prediction and target.
+
+    Args:
+        y_pred (torch.tensor): Model prediction, which is a tensor of shape (n_samples, n_class, h, w).
+        onehot_masks (torch.tensor): Groundtruth, which is a tensor of shape (n_samples, n_class, h, w).
+    Returns
+        torch.tensor: FloatTensor of shape (n_samples, n_class), which is the intersection values.
+        torch.tensor: FloatTensor of shape (n_samples, n_class), which is the union values.
+    """
+    y_pred, y_true = y_pred.float(), onehot_masks.float()
+    intersection = (y_true * y_pred).sum((2, 3))
+    union = y_true.sum((2, 3)) + y_pred.sum((2, 3)) - intersection
+    return intersection, union
+
 def IoU(y_pred, onehot_masks, threshold=0.5, eps=1e-9):
-    r'''Compute mean IoU using prediction and target'''
+    r"""Compute mean IoU using prediction and target.
+
+    Args:
+        y_pred (torch.tensor): Model prediction, which is a tensor of shape (n_samples, n_class, h, w).
+        onehot_masks (torch.tensor): Groundtruth, which is a tensor of shape (n_samples, n_class, h, w).
+    Returns
+        torch.tensor: (float) IoU value.
+    """
     y_pred, y_true = y_pred.float(), onehot_masks.float()
     intersection = (y_true * y_pred).sum((2, 3))
     union = y_true.sum((2, 3)) + y_pred.sum((2, 3)) - intersection
@@ -26,7 +64,14 @@ def IoU(y_pred, onehot_masks, threshold=0.5, eps=1e-9):
     return iou.mean()
 
 def DICE(y_pred, onehot_masks, threshold=0.5, eps=1e-9):
-    r'''Compute mean DICE using prediction and target'''
+    r"""Compute mean DICE using prediction and target.
+    
+    Args:
+        y_pred (torch.tensor): Model prediction, which is a tensor of shape (n_samples, n_class, h, w).
+        onehot_masks (torch.tensor): Groundtruth, which is a tensor of shape (n_samples, n_class, h, w).
+    Returns
+        torch.tensor: (float) DICE value.
+    """
     y_pred, y_true = y_pred.float(), onehot_masks.float()
     intersection = (y_true * y_pred).sum((2, 3))
     union = y_true.sum((2, 3)) + y_pred.sum((2, 3))

trainers/standard_trainer.py
@@ -14,9 +14,10 @@ import torch.nn.functional as F
 from torchvision import models
 
 from datasets.standard_dataset import StandardDataset
+from augmentators import DeeplabV3Augmentator
 from optimizers import get_quick_optimizer
 from metrics import CELoss, IoU, DICE
-from trainers.utils import Logger, bcolors, logits_to_onehot, init_weights
+from trainers.utils import AverageMeter, Logger, bcolors, logits_to_onehot
 from misc.voc2012_color_map import get_color_map
 
 COLOR_MAP = get_color_map()
@@ -62,9 +63,22 @@ class StandardTrainer:
         self.best_criteria, self.best_epoch = float('-inf'), -1
         self.last_criteria, self.last_epoch = float('-inf'), -1
         for epoch in range(self.config.n_epochs):
-            train_metrics = self.__train_one_epoch(epoch)
-            val_metrics = self.__eval_one_epoch(epoch)
-            self.__record_results(epoch, train_metrics, val_metrics)
+            self.train_avg_meters, self.val_avg_meters = self.__get_average_meters()
+            self.__train_one_epoch(epoch)
+            self.__eval_one_epoch(epoch)
+            self.__record_results(epoch)
+
+    def __get_average_meters(self):
+        r"""Get AverageMeter(s) for training and evaluation"""
+        train_avg_meters = {"loss": AverageMeter()}
+        train_avg_meters.update({metric: AverageMeter() for metric in self.metric_funcs})
+        train_avg_meters = EasyDict(train_avg_meters)
+        
+        val_avg_meters = {"loss": AverageMeter()}
+        val_avg_meters.update({metric: AverageMeter() for metric in self.metric_funcs})
+        val_avg_meters = EasyDict(train_avg_meters)
+        
+        return train_avg_meters, val_avg_meters
 
     def __get_optimizer(self):
         r"""Get optimizer for training segmentation model"""
@@ -94,7 +108,6 @@ class StandardTrainer:
         r"""Train single epoch"""
         self.model.train()
         progress_bar = tqdm(enumerate(self.train_loader), total=len(self.train_loader))
-        metric_progress = []
         for step, batch in progress_bar:
             images = batch["image"].to(self.config.device)
             onehot_masks = batch["onehot_mask"].to(self.config.device)
@@ -104,13 +117,12 @@ class StandardTrainer:
             
             onehot_pred_masks = logits_to_onehot(logits)
             metrics = {metric: self.metric_funcs[metric](onehot_pred_masks, onehot_masks) for metric in self.metric_funcs}
+            metrics = self.__parse_metrics(loss, metrics)
+            
             self.__update_model_params(loss)
-
-            metric_progress.append(self.__parse_metrics(loss, metrics))
-            metrics = self.__get_mean(metric_progress)
-            self.__display_progress_bar(progress_bar, "Train", epoch, step, metrics)
-        self.logger.list_of_scalars_summary([], epoch) # TODO
-        return metrics
+            self.__update_average_meters(self.train_avg_meters, metrics)
+            self.__display_progress_bar(progress_bar, "Train", epoch, step, self.train_avg_meters)
+        # self.logger.list_of_scalars_summary([], epoch) # TODO
     
     def __update_model_params(self, loss):
         r"""Run gradient descent update and learning rate update"""
@@ -123,7 +135,6 @@ class StandardTrainer:
         r"""Evaluate single epoch"""
         self.model.eval()
         progress_bar = tqdm(enumerate(self.val_loader), total=len(self.val_loader))
-        metric_progress = []
         for step, batch in progress_bar:
             images = batch["image"].to(self.config.device)
             onehot_masks = batch["onehot_mask"].to(self.config.device)
@@ -135,59 +146,58 @@ class StandardTrainer:
                 onehot_pred_masks = logits_to_onehot(logits)
                 metrics = {metric: self.metric_funcs[metric](onehot_pred_masks, onehot_masks) for metric in self.metric_funcs}
             
-            metric_progress.append(self.__parse_metrics(loss, metrics))
-            metrics = self.__get_mean(metric_progress)
-            self.__display_progress_bar(progress_bar, "Val", epoch, step, metrics)
-        self.logger.list_of_scalars_summary([], epoch) # TODO
-        return metrics
+            metrics = self.__parse_metrics(loss, metrics)
+            self.__update_average_meters(self.val_avg_meters, metrics)
+            self.__display_progress_bar(progress_bar, "Val", epoch, step, self.val_avg_meters)
+        # self.logger.list_of_scalars_summary([], epoch) # TODO
     
-    def __record_results(self, epoch, train_metrics, val_metrics):
+    def __record_results(self, epoch):
         r"""Record training results"""
         os.makedirs(self.config.checkpoint_dir, exist_ok=True)
         state_dict = {
             "model": self.model.state_dict(),
             "epoch": epoch,
-            "val_loss": val_metrics.loss,
-            "val_iou": val_metrics.iou,
-            "train_loss": train_metrics.loss,
-            "train_iou": train_metrics.iou
+            "val_loss": self.val_avg_meters.loss.average(),
+            "val_iou": self.val_avg_meters.iou.average(),
+            "train_loss": self.train_avg_meters.loss.average(),
+            "train_iou": self.train_avg_meters.iou.average()
         }
-        if self.best_criteria < val_metrics.iou:
-            path = self.__get_model_path(epoch, val_metrics.iou, best=True)
-            print(bcolors.log(f"epoch {epoch} improved from {self.best_criteria:.4f} to {val_metrics.iou:.4f} file {path}"))
+        if self.best_criteria < self.val_avg_meters.iou.average():
+            path = self.__get_model_path(epoch, self.val_avg_meters.iou.average(), best=True)
+            print(bcolors.log(f"epoch {epoch} improved from {self.best_criteria:.4f} to {self.val_avg_meters.iou.average():.4f} file {path}"))
             torch.save(state_dict, path)
             if self.best_epoch >= 0:
                 path = self.__get_model_path(self.best_epoch, self.best_criteria, best=True)
                 os.remove(path)
-            self.best_criteria, self.best_epoch = val_metrics.iou, epoch
+            self.best_criteria, self.best_epoch = self.val_avg_meters.iou.average(), epoch
         else:
-            path = self.__get_model_path(epoch, val_metrics.iou)
+            path = self.__get_model_path(epoch, self.val_avg_meters.iou.average())
             torch.save(state_dict, path)
             if self.last_epoch >= 0:
                 path = self.__get_model_path(self.last_epoch, self.last_criteria)
                 os.remove(path)
-            self.last_criteria, self.last_epoch = val_metrics.iou, epoch
+            self.last_criteria, self.last_epoch = self.val_avg_meters.iou.average(), epoch
     
     def __get_model_path(self, epoch, val_iou, best=False):
         r'''return model path with epoch number and validation iou'''
         return os.path.join(self.config.checkpoint_dir, f"{epoch:04d}_{val_iou:.4f}" + ("_best" if best else "") + ".pth")
-
-    def __get_mean(self, metric_progress):
-        r'''compute the average of metric_progress in a list of EasyDict(...) '''
-        return EasyDict({key : np.mean([x[key] for x in metric_progress]) for key in metric_progress[0]})
     
     def __parse_metrics(self, loss, metrics):
         r"""Parse loss and metrics to a dict for display purpose"""
         metrics = {metric: metrics[metric].item() for metric in metrics}
         metrics["loss"] = loss.item()
         return metrics
+    
+    def __update_average_meters(self, meter, metrics):
+        r"""Update AverageMeter"""
+        for metric in metrics:
+            meter[metric].update(metrics[metric])
 
-    def __display_progress_bar(self, progress_bar, phase, epoch, step, metrics):
+    def __display_progress_bar(self, progress_bar, phase, epoch, step, meter):
         r"""Display progress bar"""
         desc = f"{phase} epoch {epoch}/{self.config.n_epochs} step {step}: "
-        for metric in metrics:
-            metric_value = metrics[metric]
-            desc += f"{metric}: {metric_value:.4f} "
+        for metric in meter:
+            desc += f"{metric}: {meter[metric].average():.4f} "
         progress_bar.set_description(desc)
 
 
@@ -210,14 +220,18 @@ if __name__ == "__main__":
         model_config = json.load(f)
     module = importlib.import_module(args.model_module)
     model = getattr(module, "get_model")(model_config).to(args.device)
-    model.apply(init_weights)
     
+    augmentator = DeeplabV3Augmentator(
+        target_size=(512, 512),
+        crop_size=(512, 512)
+    )
     data_config = EasyDict(dict(
-        data_root=args.data_root,
-        label_map_file=args.label_map_file,
-        augment_data=None,
+        data_root="/home/cotai/giang/datasets/VOC-2012",
+        label_map_file="/home/cotai/giang/datasets/VOC-2012/label_map.json",
+        augment_data=augmentator,
         preprocess=None,
-        target_size=(256, 256)
+        target_size=(512, 512),
+        ignored_class=21
     ))
     train_config = EasyDict(dict(
         model=model,

trainers/utils.py
@@ -20,9 +20,9 @@ class AverageMeter(object):
             weight (float): Weight assigned to the initial value to carry out average and summation.
         """
         if not self.initialized:
-            self.initialize(val, weight)
+            self.__initialize(val, weight)
         else:
-            self.add(val, weight)
+            self.__add(val, weight)
 
     def __initialize(self, val, weight):
         r"""Initialize Average meter by a value"""
@@ -81,17 +81,6 @@ class bcolors:
     def log(x):
         return f"{bcolors.BOLD}{bcolors.OKBLUE}{x}{bcolors.ENDC}{bcolors.ENDC}"
 
-
-def init_weights(m):
-    r'''init weights of a layer with xavier initialization'''
-    if type(m) == torch.nn.Linear:
-        torch.nn.init.xavier_uniform_(m.weight)
-        if getattr(m, "bias") is not None:
-            m.bias.data.fill_(0.01)
-    elif type(m) == torch.nn.Conv2d:
-        torch.nn.init.xavier_uniform_(m.weight)
-        if getattr(m, "bias") is not None:
-            m.bias.data.fill_(0.01)
 
 def logits_to_onehot(logits):
     probs = F.softmax(logits, dim=1)